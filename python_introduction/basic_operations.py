814e1e42f34ce981190c14509fcde1dccaa66904
hamazin <hademolahamazin@gmail.com>
2025-07-11 08:11:20 +0100

    first commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+Addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+print(Addition)
+print(subtraction)
+print(multiplication)
---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,4 @@
+user_current_age = input("how old are you")
+user_age = 27
+user_future_age = user_age * User_current_age
+print(user_future_age)
